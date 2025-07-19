1957c2a8793e50d7248b6bce697c5a850ff29748
zero0043 <wwzerome@gmail.com>
2025-07-20 02:44:23 +0800

    fix(clean): Remove unused .commitLog/ directory

--- clean.sh
+++ clean.sh
@@ -3,7 +3,7 @@ rm -rf build/
 rm -rf dist/
 rm -rf ai_commit.egg-info/
 rm -rf .pytest_cache/
-rm -rf .commitLog/
+
 
 # Remove Python cache files (if any)
 find . -type d -name "__pycache__" -exec rm -rf {} + 2>/dev/null || true
